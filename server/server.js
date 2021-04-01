58a853496b43e43eca7753c3095153d26a6563cb
Leo Bonato <leo.bonato@protonmail.com>
2021-04-01 17:35:18 +0200

    y u force me to push?

--- server/server.js
+++ server/server.js
@@ -1,16 +1,56 @@
 const express = require("express");
-const app = express();
 const compression = require("compression");
 const path = require("path");
+const cookieSession = require("cookie-session");
+const csurf = require("csurf");
+
+const succeed = (res) => res.json({ success: true });
+const fail = (res) => res.json({ success: false });
+const staticize = (file) => path.join(__dirname, "..", "client", file);
+
+// ================================ INIT ================================ //
+const app = express();
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(
+    cookieSession({
+        secret: process.env.SESSION_SECRET || "whatever",
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+    })
+);
+
+app.use(csurf());
+app.use(function (req, res, next) {
+    res.cookie("csrf_token", req.csrfToken());
+    next();
+});
+
+app.use(express.static(staticize("public")));
+
+app.get("/welcome", ifLogged("in", "/"), (req, res) =>
+    res.sendFile(staticize("index.html"))
+);
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+    res.sendFile(staticize("index.html"));
 });
 
 app.listen(process.env.PORT || 3001, function () {
     console.log("I'm listening.");
 });
+
+// ================================ MIDDLEWARE ================================ //
+function ifLogged(state, redirection) {
+    return (req, res, next) => {
+        const test = state == "in" ? req.session.userId : !req.session.userId;
+        if (test) return res.redirect(redirection);
+        return next();
+    };
+}
