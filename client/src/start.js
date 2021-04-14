2d8560216fe115de29e59e3a17a90c44a2af579a
Leo Bonato <leo.bonato@protonmail.com>
2021-04-14 10:19:18 +0200

    redux

--- client/src/start.js
+++ client/src/start.js
@@ -2,12 +2,28 @@ import ReactDOM from "react-dom";
 import Welcome from "./welcome";
 import App from "./app";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import reducer from "./reducer";
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
+
 let elem;
 
-if (location.pathname === "/welcome") {
+if (location.pathname == "/welcome") {
     elem = <Welcome />;
 } else {
-    elem = <App />;
+    elem = (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 }
 
 ReactDOM.render(elem, document.querySelector("main"));
